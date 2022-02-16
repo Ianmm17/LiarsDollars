e0266fc87ec010e3e2c0e5a38f435ffeec395508
Miller <milleri@wwt.com>
2022-02-16 09:12:57 -0600

    adds remove-player button to player-card

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 // Get the element
 let numberOfPlayers = 1;
+let playerCard = ""
+
 function addPlayer() {
     numberOfPlayers += 1
-    let playerCard = `player-card ${numberOfPlayers}`
+    playerCard = `player-card${numberOfPlayers}`
     console.log("SORTA ADDED PLAYER")
     let elementPlayerClasses = document.getElementById("player-classes")
-    elementPlayerClasses.insertAdjacentHTML("afterend", "" +
+    elementPlayerClasses.insertAdjacentHTML("beforeend", "" +
         `<div class=${playerCard} id=${playerCard}>` +
-                `<input id="player-name" class="player-name" type='text' name='player-name' maxlength=12 placeholder="Enter Player Name" color=\"grey\"/>` +
-                `<input class="player-total" type='number' name='player-total' maxlength=12/><br>` +
-                `<button class="winner" type="button" name="winner">W</button>` +
-                `<button class="loser" type="button" name="loser">L</button>` +
-        `</div>`)
+                `<input id="player-name" class="player-name" type='text' name='player-name' maxlength=12 placeholder="Enter Player Name" color=\"grey\"/>
+                <input class="player-total" type='number' name='player-total' maxlength=12/>
+                <button class="remove-selected-player" type="button" name="remove-selected-player" onclick="removePlayer(playerCard)">X</button><br>
+                <button class="winner" type="button" name="winner">W</button>
+                <button class="loser" type="button" name="loser">L</button>
+        </div>`)
     //elementPlayerClasses.append(clone)
-    console.log(numberOfPlayers)
+    console.log(playerCard)
+}
+
+function removePlayer(playerCardNum) {
+    console.log("did it work?", playerCardNum)
+
+    document.getElementById(playerCardNum).remove()
 }
 

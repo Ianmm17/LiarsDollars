c69ef4e49cb2500765819e8986241c9ffc7905f9
Miller <milleri@wwt.com>
2022-02-16 12:49:14 -0600

    adds functionality to addPlayer() and creates resetGame() to clear game to it's intial state

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // Get the element
 let numberOfPlayers = 1;
-let playerCard = ""
+
 
 function addPlayer() {
-    numberOfPlayers += 1
+    let playerCard = ""
+    if (document.getElementById("player-card").valueOf() === "player-card") {
+        numberOfPlayers = 2
+    } else {
+        numberOfPlayers += 1
+    }
     playerCard = `player-card${numberOfPlayers}`
     console.log("SORTA ADDED PLAYER")
     let elementPlayerClasses = document.getElementById("player-classes")
-    elementPlayerClasses.insertAdjacentHTML("beforeend", "" +
+    elementPlayerClasses.insertAdjacentHTML("beforeend",
         `<div class=${playerCard} id=${playerCard}>` +
                 `<input id="player-name" class="player-name" type='text' name='player-name' maxlength=12 placeholder="Enter Player Name" color=\"grey\"/>
                 <input class="player-total" type='number' name='player-total' maxlength=12/>
@@ -16,5 +21,21 @@ function addPlayer() {
         </div>`)
     //elementPlayerClasses.append(clone)
     console.log(playerCard)
+}
+
+function resetGame() {
+    numberOfPlayers = 1
+    //window.location.reload();
+    document.getElementById("player-classes").remove()
+    document.getElementById("add-or-remove-players").insertAdjacentHTML("afterend",
+        `<div class="player-classes" id="player-classes">
+            <div class="player-card" id="player-card">
+            <input id="player-name" class="player-name" type='text' name='player-name' maxlength=12 placeholder="Enter Player Name" color="grey"/>
+            <input class="player-total" type='number' name='player-total' maxlength=12/>
+            <button class="winner" type="button" name="winner">W</button>
+            <button class="loser" type="button" name="loser">L</button>
+            </div>
+        </div>`
+    )
 }
 

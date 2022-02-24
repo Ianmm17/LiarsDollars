765a18d56d80454f78f11f4cf4df29a46b3f9f84
ian miller <ianmm17@yahoo.com>
2022-02-23 18:50:34 -0600

    WIP on w button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let numberOfPlayers = 1;
 let playerName = "player-name"
 let playerTotal = "player-total"
 let playerArr = ['player-total']
+//document.querySelectorAll('.player-class .player-name') maybe use?
 
 
 function addPlayer() {
@@ -32,10 +33,10 @@ function resetGame() {
     document.getElementById("add-or-reset").insertAdjacentHTML("afterend",
         `<div class="player-classes" id="player-classes">
             <div class="player-card" id="player-card">
-            <input id="player-name" class="player-name" type='text' name='player-name' maxlength=12 placeholder="Enter Player Name" color="grey"/>
-            <input class="player-total" type='number' name='player-total' maxlength=12/>
-            <button class="winner" type="button" name="winner" onclick="winner(document.getElementById('player-name').value)">W</button>
-            <button class="loser" type="button" name="loser">L</button>
+                <input id="player-name" class="player-name" type='text' name='player-name' maxlength=12 placeholder="Enter Player Name" color="grey"/>
+                <input id="player-total" class="player-total" type='number' name='player-total' maxlength=12/>
+                <button class="winner" type="button" name="winner" onclick="winner('player-name', 'player-total')">W</button>
+                <button class="loser" type="button" name="loser">L</button>
             </div>
         </div>`
     )
@@ -44,8 +45,8 @@ function resetGame() {
         `<table id="overall-table">
             <tr id="table-headers">
                 <th>Player</th>
-                <th>Win/Loss</th>
-                <th>Amount Won/Loss</th>
+                <th>Amount Won/Lost</th>
+                <th>Win/Lost</th>
             </tr>
             <tr id="table-data">
                 <td>Alfreds Futterkiste</td>
@@ -67,17 +68,20 @@ function winner(playerName, playerBalance) {
     } else {
         let newPlayerBalance = Number(balance) + (numberOfPlayers -1)
         document.getElementById(playerBalance).value = newPlayerBalance
-        document.getElementById("table-headers").insertAdjacentHTML("afterend",
+        addTableRow(name, numberOfPlayers -1)
+    }
+
+}
+
+function addTableRow(name, moneyEarned) {
+    document.getElementById("table-headers").insertAdjacentHTML("afterend",
             `<tr id="table-data">
             <td>${name}</td>
-            <td>+$${numberOfPlayers - 1}.00</td>
+            <td>+$${moneyEarned}.00</td>
             <td>Germany</td>
             <td id="remove-record"><button class="remove-record" onclick="removeData()">X</button></td>
         </tr>`
         )
-
-    }
-
 }
 
 function removeData() {
